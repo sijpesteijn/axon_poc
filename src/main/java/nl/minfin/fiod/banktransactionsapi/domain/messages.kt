36cbaef3a068820f238cdb127c0f3e24ab6e172f
gijs.sijpesteijn <gijs@jpoint.nl>
2020-09-24 09:28:12 +0200

    step 0

--- src/main/java/nl/minfin/fiod/banktransactionsapi/domain/messages.kt
+++ src/main/java/nl/minfin/fiod/banktransactionsapi/domain/messages.kt
@@ -2,10 +2,7 @@ package nl.minfin.fiod.banktransactionsapi.domain
 
 import nl.minfin.fiod.banktransactionsapi.banktransactions.ParseStatus
 import org.axonframework.modelling.command.TargetAggregateIdentifier
-import java.time.Instant
 
-
-data class AuditEvent(val timestamp: Instant, val payLoad: Object)
 data class CreateBankTransactionCommand(@TargetAggregateIdentifier val bankTransactionId: String, val bankTransaction: BankTransactionDto)
 data class UpdateBankTransactionParseStatusCommand(@TargetAggregateIdentifier val bankTransactionId: String, val parseStatus: ParseStatus)
 
